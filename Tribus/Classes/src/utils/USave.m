1ed5fdd2f620cdc827b98f2c5b2c7686ea3eba1a
lbineau <lory.bineau@gmail.com>
2012-05-17 19:41:20 +0200

    Add a button for reset app

---
+++ Tribus/Classes/src/utils/USave.m
@@ -0,0 +1,13 @@
+//
+//  USave.m
+//  Tribus
+//
+//  Created by lbineau on 17/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "USave.h"
+
+@implementation USave
+
+@end
